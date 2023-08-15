86100dbfaf79023c904898c41bae60bcceb8b810
jorgevictorsantos <jorge_santos@usp.br>
2023-08-15 16:36:56 -0300

    aula-2

--- aula-2.R
+++ aula-2.R
@@ -2,17 +2,19 @@
 
 # pacotes
 install.packages("data.table")
+install.packages('ribge')
+
 library(data.table)
-install.packages("janitor")
-library(janitor)
-install.packages(c("knitr", "kableExtra"))
-library(knitr)
-library(kableExtra)
+
+library(tidyverse)
+library(stringr)
 
 
 # vamos importar uma base de dados de pib municipais de 2013, do IBGE
 
 # o arquivo está em formato RDS, que é um formato do R, e disponível no meu github. Para importá-lo direto no R, vamos ler o arquivo com a função url e depois import´-lo com a função readRDS. 
+
+# comando url antes para conseguir puxar a base
 
 pib_cid <- readRDS(url("https://github.com/mgaldino/book-regression/raw/main/dados/pib_cid.RDS"))
 
@@ -28,13 +30,60 @@ head(pib_cid)
 
 View(pib_cid)
 
-library(tidyverse)
 
 # digamos que quero o pib total médio e o pib per capita médio
 # basta usar o comando summarise, que resume os dados e escolher a função mean.
 
-df <- pib_cid %>%
+df <- pib_cid %>% 
   summarise(pib_medio = mean(pib_total),
             pib_per_capita_medio = mean(pib_per_capita))
 
-kable(df)
+
+# se eu quiser a soma dos pibs municipais
+df <- pib_cid %>%
+  summarise(soma_pib = sum(pib_total)) %>%
+  head()
+
+pib_cid %>%
+  filter(sigla_uf == "SP") %>% 
+  filter(str_detect(nome_munic, "São")== T) %>% 
+  group_by(nome_munic) %>% 
+  reframe(pib_cidade = sum(pib_total)) %>% 
+  View()
+
+
+
+# maior e menos pibs e pibs per capita entre municípios
+df <- pib_cid %>%
+  summarise(pib_max = max(pib_total),
+            pib_min = min(pib_total),
+            pib_per_capita_max = max(pib_per_capita),
+            pib_per_capita_min = min(pib_per_capita))
+
+
+df <- pib_cid %>%
+  mutate(pib_max = max(pib_total),
+         pib_min = min(pib_total),
+         pib_per_capita_max = max(pib_per_capita),
+         pib_per_capita_min = min(pib_per_capita)) %>%
+  filter(pib_total == pib_max
+         | pib_total == pib_min 
+         | pib_per_capita == pib_per_capita_max
+         | pib_per_capita == pib_per_capita_min)
+
+df <- pib_cid %>%
+  mutate(pib_max = max(pib_total),
+         pib_min = min(pib_total),
+         pib_per_capita_max = max(pib_per_capita),
+         pib_per_capita_min = min(pib_per_capita)) %>%
+  filter(pib_total %in% c(pib_max,pib_min) |
+           pib_per_capita %in% c(pib_per_capita_max,pib_per_capita_min))
+
+
+# agora, quero criar uma nova variável, que é o pib estadual
+df <- pib_cid %>%
+  group_by(sigla_uf) %>%
+  summarise(pib_uf = sum(pib_total),
+            pib_medio_uf = mean(pib_total),
+            pib_uf_mediana = median(pib_total))
+
