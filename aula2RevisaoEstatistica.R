c3f8d6b4475efbe0a9eb21c30aae946f5b1dd03b
jorgevictorsantos <jorge_santos@usp.br>
2023-08-15 17:28:44 -0300

    revisão

---
+++ aula2RevisaoEstatistica.R
@@ -0,0 +1,31 @@
+# Revisão de estatística
+
+# Medidas de tendência central
+
+# Média
+# média amostral
+
+a <- c(1:10)
+b <- c(rep(5, 5))
+c <- c(1, 3, 5, 7, 9, 11)
+d <- c(-5:5)
+
+mean(a)
+(sum(a)/length(a))
+
+mean(b)
+mean(c)
+mean(d)
+
+
+# variável aleatória
+
+# variável aleatória discreta: somente valores finitos
+# variável aleatória contínua: apresenta infinitos valores
+
+# Esperança matemática: média de uma variável aleatória
+
+# Variância na variável aleatória: desvios em torno da esperança
+
+
+
